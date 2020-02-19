0f88073035ac95d9a11939b52097481f6c75d3ae
katherinekimes <57684996+katherinekimes@users.noreply.github.com>
2020-02-19 19:40:13 +0000

    Done with Game

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,33 @@
-
 import './styles.css';
-console.log('Ready to Party With Some TypeScript!');
 
+const secretNumber = Math.floor(Math.random() * 10);
+
+const squares = document.querySelectorAll('.square') as NodeListOf<HTMLDivElement>;
+
+let currentSquare = Math.floor(Math.random() * 10);
+
+squares.forEach(sq => {
+    if (currentSquare === secretNumber) {
+        sq.dataset.secret = 'true';
+
+    }
+    currentSquare++;
+    sq.addEventListener('click', handleClick);
+});
+
+function handleClick() {
+    const that = this as HTMLDivElement;
+    if (that.dataset.secret) {
+        that.classList.add('winner');
+        that.removeEventListener('click', handleClick);
+
+        squares.forEach(sq => {
+            if (sq !== that) {
+                sq.classList.add('loser');
+            }
+        });
+    } else {
+        that.classList.add('loser');
+        that.removeEventListener('click', handleClick);
+    }
+}
